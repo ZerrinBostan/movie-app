ee35fc7adb8039c7d6c12dbef1e48651627f3542
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2019-06-19 23:19:12 +0300

    Key made unique

--- components/imdbItems.js
+++ components/imdbItems.js
@@ -9,7 +9,14 @@ export class ImdbItems extends Component {
         return (
             <div className="d-flex flex-row justify-content-between flex-wrap">
                 {
-                    imdbStore.jsonArray.map((json, index) => <ImdbItem src={json.Poster} title={json.Title} key={index} year={json.Year} point={json.imdbRating} imdbID={json.imdbID} />)
+                    imdbStore.jsonArray.map((json, index) => 
+                    <ImdbItem 
+                    src={json.Poster} 
+                    title={json.Title}
+                     key={json.imdbID} 
+                     year={json.Year} 
+                     point={json.imdbRating} 
+                     imdbID={json.imdbID} />)
                 }
             </div>
         )
