ccf9d8d9dd5b521c46a26d87e335ed6eff544eb5
ZerrinBostan <46298014+ZerrinBostan@users.noreply.github.com>
2019-06-19 17:40:45 +0300

    Added obj for favoriteIcon comp.

--- components/imdbItem/imdbItem.js
+++ components/imdbItem/imdbItem.js
@@ -7,13 +7,20 @@ import FavoriteIcon from './favoriteIcon';
 
 export class ImdbItem extends Component {
     render() {
+        const favoriteObj = {
+            src: this.props.src,
+            title: this.props.title,
+            point: this.props.point,
+            imdbID: this.props.imdbID,
+            year: this.props.year
+        }
         return (
             <div className="card mb-3">
                 <ImdbPoster src={this.props.src} />
                 <div className="card-body">
                     <div className="d-flex flex-row justify-content-between">
                             <ImdbTitle title={this.props.title} />
-                            <FavoriteIcon imdbID={this.props.imdbID}/>
+                            <FavoriteIcon favoriteItemObj={favoriteObj}/>
                     </div>
                     <ImdbPoint point={this.props.point} />
                     <ImdbYear year={this.props.year} />
